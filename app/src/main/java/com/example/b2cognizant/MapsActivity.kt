ace8f23056b21f89b6f45d725dd0e2a67ed12faf
abdul ansari <abdul.tanveer@gmail.com>
2022-08-03 22:02:09 +0530

    facebook sso

--- app/src/main/java/com/example/b2cognizant/MapsActivity.kt
+++ app/src/main/java/com/example/b2cognizant/MapsActivity.kt
@@ -41,8 +41,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        val sydney = LatLng(17.4481, 78.3760)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+
     }
 }
---
+++ app/src/main/java/com/example/b2cognizant/sso/SsoActivity.java
@@ -0,0 +1,65 @@
+package com.example.b2cognizant.sso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.b2cognizant.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class SsoActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
+    private static final String EMAIL = "email";
+    LoginButton loginButton;
+    public static String TAG = SsoActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sso);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i(TAG,"the token is--"+loginResult.getAccessToken());
+                //start another activity or another fragment
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
+}
