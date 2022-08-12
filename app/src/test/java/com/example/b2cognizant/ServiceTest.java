572b7c80380f1bf0f98c136d80e965ee4e48df2c
abdul ansari <abdul.tanveer@gmail.com>
2022-08-12 19:29:53 +0530

    mockito

---
+++ app/src/test/java/com/example/b2cognizant/ServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.b2cognizant;
+
+
+import com.example.b2cognizant.mockito.Database;
+import com.example.b2cognizant.mockito.Service;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+
+@ExtendWith(MockitoExtension.class)
+public class ServiceTest {
+
+    @Mock
+    Database databaseMock;
+
+    @Test
+    public void testQuery()  {
+        assertNotNull(databaseMock);
+        when(databaseMock.isAvailable()).thenReturn(true);
+        Service t  = new Service(databaseMock);
+        boolean check = t.query("* from t");
+        assertTrue(check);
+    }
+
+}
